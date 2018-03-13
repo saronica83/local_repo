80c763e58103528e4744e56a8fc3bf94f0e28ed5
saronica83 <sarodriguez83@hotmail.com>
2018-03-13 04:56:41 -0600

    Mi segunda modificación

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var x={
     nombre:"Sergio Antonio Rodriguez Ocon",
     lugar:"Managua Nicaragua",
     profesion:"Super Ingeniero en computación",
-    edad:"35"
+    edad:"35",
+    estado_civil:"Soltero"
 }
 
-console.log(x.nombre + " " + x.lugar + " " + x.profesion + " " + "Cumpliré " + x.edad);
+console.log(x.nombre + " " + x.lugar + " " + x.profesion + " " + "Cumpliré " + x.edad + " " + "Estado Civil " + x.estado_civil);
