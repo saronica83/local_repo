298a3071a694c3ba11d145f822f5343176908bcf
saronica83 <sarodriguez83@hotmail.com>
2018-03-13 04:44:51 -0600

    Enviando desde mi maquina local

---
+++ app.js
@@ -0,0 +1,7 @@
+var x={
+    nombre:"Sergio Antonio Rodriguez Ocon",
+    lugar:"Managua Nicaragua",
+    profesion:"Super Ingeniero en computación"
+}
+
+console.log(x.nombre + " " + x.lugar + " " + x.profesion);
